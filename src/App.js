e8d6889864401cfde29defb616bf9da939330ed7
JesusManuelMallo <88166930+JesusManuelMallo@users.noreply.github.com>
2021-09-21 21:35:43 +0200

    Iterat2, Error App.js linea 30
    Yo he puesto lo que hay, y me imprime en localhost el trofeo, pero con el interrogante detraas y los dos puntos despues visibles.. Ivan me aconsejó esto <td> {contact.wonOscar && <p>:trophy:</p>}</td> que tampoco iba, y sobre como tengo la linea 30, Aitor me decia poner un } antes de cerrar td, que tampoco. Todo tras documentarme aquí https://desarrolloweb.com/articulos/condicionales-templates-jsx-react.html

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
 import logo from "./logo.svg";
 
-import contacts from "contacts,json"; //ITERATION1 pass1: import contacts
+import contactsList from "./contacts.json"; //ITERATION1 pass1: import contacts
 
 import { useState } from "react";
 
 function App() {
-  const [contacts, setContacat] = useState(contacts.slice(0, 5)); //Iter1 pas1 extrayendo 5 elementos del array
+  const [contacts, setContacts] = useState(contactsList.slice(0, 5)); //Iter1 pas1 extrayendo 5 elementos del array
   return (
     <div className="App">
       <table>
-        {contacts.map((contact) => {
-          return console.log(contact);
-        })}
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won an Oscar</th>
+            <th>Won an Emmy</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contacts.map((contact) => {
+            return (
+              <tr key={contact.id}>
+                <td>
+                  <img src={contact.pictureUrl} alt={contact.name} />
+                </td>
+                <td>{contact.name}</td>
+                <td>{contact.popularity}</td>
+                <td> {contact.wonOscar} ? "🏆" : ""</td>
+              </tr>
+            );
+          })}
+        </tbody>
       </table>
     </div>
   );
